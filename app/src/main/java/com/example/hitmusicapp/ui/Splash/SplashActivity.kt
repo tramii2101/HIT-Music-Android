78f315bca3804839899b82b9c8e0515950248609
LAPTOP-87B55DHQ\TRA MI <mi21012003@gmail.com>
2023-07-04 19:11:00 +0700

    Add splash screen

---
+++ app/src/main/java/com/example/hitmusicapp/ui/Splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hitmusicapp.ui.Splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.hitmusicapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
--- app/src/main/java/com/example/hitmusicapp/ui/authentication/forgot_methods/ForgotPasswordMethodsFragment.kt
+++ app/src/main/java/com/example/hitmusicapp/ui/authentication/forgot_methods/ForgotPasswordMethodsFragment.kt
@@ -2,12 +2,16 @@ package com.example.hitmusicapp.ui.authentication.forgot_methods
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
 import com.example.hitmusicapp.base.BaseFragment
 import com.example.hitmusicapp.databinding.FragmentForgotPasswordMethodsBinding
 
 class ForgotPasswordMethodsFragment : BaseFragment<FragmentForgotPasswordMethodsBinding>() {
-    override fun initListener() {
 
+    override fun initListener() {
+        binding.tvEmailMethod.setOnClickListener {
+
+        }
     }
 
     override fun initView() {
