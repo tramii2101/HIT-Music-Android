9ad84f24d03db9363a953b1844f90255b846f7c8
= <ledung2020604439@gmail.com>
2023-07-11 19:15:05 +0700

    changes version material, add library circleimageview, add ViewPagerAdapter,add YourProfileFragment, add ExploreFragment,HomeFragment,LibraryFragment,ProfileFragment, add logic MainActivity, add menu_btn_nav, add color_btn_nav, add item colors,string,icon

---
+++ app/src/main/java/com/example/hitmusicapp/screen/home/fragment/HomeFragment.kt
@@ -0,0 +1,36 @@
+package com.example.hitmusicapp.screen.home.fragment
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.hitmusicapp.base.BaseFragment
+import com.example.hitmusicapp.databinding.FragmentHomeBinding
+
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>() {
+    override fun initListener() {
+
+    }
+
+    override fun initView() {
+
+    }
+
+    override fun initData() {
+
+    }
+
+    override fun handleEvent() {
+
+    }
+
+    override fun bindData() {
+
+    }
+
+    override fun inflateLayout(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentHomeBinding {
+        return FragmentHomeBinding.inflate(inflater, container, false)
+    }
+}
