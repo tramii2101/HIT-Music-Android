e61a1afa0c44eee023ebbb1c41d40f21723f7927
LAPTOP-87B55DHQ\TRA MI <mi21012003@gmail.com>
2023-07-13 10:48:22 +0700

    Add ExploreFragment

---
+++ app/src/main/java/com/example/hitmusicapp/screen/home/fragment/ExploreFragment.kt
@@ -0,0 +1,36 @@
+package com.example.hitmusicapp.screen.home.fragment
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.hitmusicapp.base.BaseFragment
+import com.example.hitmusicapp.databinding.FragmentExploreBinding
+
+class ExploreFragment : BaseFragment<FragmentExploreBinding>() {
+    override fun initListener() {
+
+    }
+
+    override fun initView() {
+
+    }
+
+    override fun initData() {
+
+    }
+
+    override fun handleEvent() {
+
+    }
+
+    override fun bindData() {
+
+    }
+
+    override fun inflateLayout(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentExploreBinding {
+        return FragmentExploreBinding.inflate(inflater, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/example/hitmusicapp/screen/home/fragment/HomeFragment.kt
@@ -0,0 +1,57 @@
+package com.example.hitmusicapp.screen.home.fragment
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hitmusicapp.adapter.PopularSongAdapter
+import com.example.hitmusicapp.adapter.SingerAdapter
+import com.example.hitmusicapp.base.BaseFragment
+import com.example.hitmusicapp.databinding.FragmentHomeBinding
+import com.example.hitmusicapp.utils.extension.setLinearLayoutManager
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>() {
+    private val singerAdapter by lazy {
+        SingerAdapter()
+    }
+
+    private val popularSongAdapter by lazy {
+        PopularSongAdapter()
+    }
+
+    override fun initListener() {
+
+    }
+
+    override fun initView() {
+        binding.recyclerSinger.setLinearLayoutManager(
+            requireContext(),
+            singerAdapter,
+            RecyclerView.HORIZONTAL
+        )
+        binding.recyclerTrend.setLinearLayoutManager(
+            requireContext(),
+            popularSongAdapter,
+            RecyclerView.HORIZONTAL
+        )
+    }
+
+    override fun initData() {
+
+    }
+
+    override fun handleEvent() {
+
+    }
+
+    override fun bindData() {
+
+    }
+
+    override fun inflateLayout(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentHomeBinding {
+        return FragmentHomeBinding.inflate(inflater, container, false)
+    }
+
+}
