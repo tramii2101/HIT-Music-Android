2d2ccba8f2509edc4e4400dbfbefed4e27661f62
LAPTOP-87B55DHQ\TRA MI <mi21012003@gmail.com>
2023-07-20 17:55:52 +0700

    Add UI of FavouriteSongFragment, add value in strings.xml

---
+++ app/src/main/java/com/example/hitmusicapp/screen/home/favourite/FavouriteSongFragment.kt
@@ -0,0 +1,36 @@
+package com.example.hitmusicapp.screen.home.favourite
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.hitmusicapp.base.BaseFragment
+import com.example.hitmusicapp.databinding.FragmentFavouriteSongBinding
+
+class FavouriteSongFragment : BaseFragment<FragmentFavouriteSongBinding>() {
+    override fun initListener() {
+
+    }
+
+    override fun initView() {
+
+    }
+
+    override fun initData() {
+
+    }
+
+    override fun handleEvent() {
+
+    }
+
+    override fun bindData() {
+
+    }
+
+    override fun inflateLayout(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentFavouriteSongBinding {
+        return FragmentFavouriteSongBinding.inflate(inflater, container, false)
+    }
+
+}
