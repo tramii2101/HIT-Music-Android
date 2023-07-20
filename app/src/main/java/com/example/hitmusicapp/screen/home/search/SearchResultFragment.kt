cf59f34d579e5e3da078e05312c8372f0b24d873
LAPTOP-87B55DHQ\TRA MI <mi21012003@gmail.com>
2023-07-20 11:27:11 +0700

    Add UI of SearchFragment, item_song.xml, color_filter_background.xml, item_result_singer.xml

---
+++ app/src/main/java/com/example/hitmusicapp/screen/home/search/SearchResultFragment.kt
@@ -0,0 +1,37 @@
+package com.example.hitmusicapp.screen.home.search
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.hitmusicapp.base.BaseFragment
+import com.example.hitmusicapp.databinding.FragmentSearchResultBinding
+
+
+class SearchResultFragment : BaseFragment<FragmentSearchResultBinding>() {
+    override fun initListener() {
+
+    }
+
+    override fun initView() {
+
+    }
+
+    override fun initData() {
+
+    }
+
+    override fun handleEvent() {
+
+    }
+
+    override fun bindData() {
+
+    }
+
+    override fun inflateLayout(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentSearchResultBinding {
+        return FragmentSearchResultBinding.inflate(inflater, container, false)
+    }
+
+}
